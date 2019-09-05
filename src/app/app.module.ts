941144f51fa4d5b746544f0e3da80191ecd16b5e
liguangyi <252438823@qq.com>
2019-09-05 23:49:58 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoComponent } from './demo/demo.component';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { DemoComponent } from './demo/demo.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -17,8 +23,14 @@ import { DemoComponent } from './demo/demo.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
