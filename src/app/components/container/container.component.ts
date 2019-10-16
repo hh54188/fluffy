cc403de97eff36238cc666017671a58095a49565
liguangyi <252438823@qq.com>
2019-10-16 23:50:52 +0800

    update

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { Observable } from 'rxjs';
 import { BookmarkItemContextMenuComponent } from '../bookmark-item-context-menu/bookmark-item-context-menu.component';
 import { ContextMenuService } from '../../services/context-menu/context-menu.service'
 import { ContextMenuTypes } from '../../enums/context-menu-types';
@@ -11,14 +12,24 @@ import { ContextMenuState } from '../../models/context-menu-state';
 })
 export class ContainerComponent implements OnInit {
   constructor(private contextMenuService: ContextMenuService) {
-    this.contextMenuService.getContextMenuState().subscribe(contextMenuState => {
+    // this.contextMenuService.getContextMenuState().subscribe(contextMenuState => {
+    //   console.log(contextMenuState);
+    // })
+    const subject = this.contextMenuService.getContextMenuState();
+    // subject.subscribe(e => console.log("!!"));
+    new Observable(observer => {
+      subject.subscribe(e => console.log("!!"));
+      observer.next("haha")
+      // this.contextMenuService.getContextMenuState().subscribe(contextMenuState => {
+      //   console.log(contextMenuState);
       // this.contextMenuState = contextMenuState
       // if (contextMenuState.show) {
       //   this.contextMenu.openContextMenu(contextMenuState.position);
       // } else {
       //   this.contextMenu.closeContextMenu();
       // }
-    });
+      // });
+    }).subscribe(e => console.log("??"))
   }
 
   private contextMenuState: ContextMenuState;
